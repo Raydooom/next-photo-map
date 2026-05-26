a66b529d496302172ff3f828f4874b919d231543
raydom <raydom007@163.com>
2026-05-26 11:14:48 +0800

    refactor: 优化照片展示组件与页面布局

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -6,9 +6,9 @@ export const dynamic = 'force-dynamic';
 export default async function DocsPage() {
   const { list } = await PhotoAction.getPhotoList();
   return (
-    <div className="min-h-screen px-4">
+    <div className="min-h-screen px-6 py-6 md:px-8 md:py-8">
       <Suspense>
-        <MasonryGrid items={list} columns={6} />
+        <MasonryGrid items={list} columns={5} />
       </Suspense>
     </div>
   );
